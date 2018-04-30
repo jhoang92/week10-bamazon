a09ba111cd6803e33fbeb7718280eef79a7b63fb
jhoang92 <jhhoang992@gmail.com>
2018-04-29 22:44:28 -0600

    established connection in customer CLI

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # week10-bamazon
+This application is made to show some of the MySQL skills that has been recently acquired. The app will allow customers to "buy" items and update the stores inventory. The app runs as a CLI.
+
+# NPM packages 
+This app contains the following NPM packages:
+`MySQL`
+`Inquirer`
---
+++ bamazonCustomer.js
@@ -0,0 +1,26 @@
+var inquire = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    console.log("ID Connected: " + connection.threadId);
+    readProduct();
+});
+
+function readProduct() {
+    console.log("Inventory search...\n");
+    connection.query('SELECT * FROM products', function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+};
