013cca8b85563000e72c54aca7004bca2d101d51
jhoang92 <jhhoang992@gmail.com>
2018-05-02 16:30:13 -0600

    added most functionaility need to fix memory leak

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-var inquire = require('inquirer');
+var inquirer = require('inquirer');
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -10,17 +10,52 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
-    console.log("ID Connected: " + connection.threadId);
-    readProduct();
-});
+function connect() {
+    connection.query('SELECT * FROM products', function (err, res) {
+        for (var i = 0; i < res.length; i++) {
 
-function readProduct() {
-    console.log("Inventory search...\n");
-    connection.query('SELECT * FROM products', function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    });
-};
+            if (err) throw err;
+            console.log('----------------');
+            console.log('Product ID: ' + res[i].id + ' Product Name: ' + res[i].product_name + ' Department: ' + res[i].department_name + ' Price: ' + res[i].price + ' quantity: ' + res[i].stock_quantity);
+            console.log('----------------');
+            // options();
+
+            inquirer.prompt([
+                {
+                    name: 'purchase',
+                    type: 'input',
+                    message: 'input the ID of the item you want to buy'
+                }, {
+                    name: 'quantity',
+                    type: 'input',
+                    message: 'how much would you like to purchase?'
+                }
+            ]).then(function (input) {
+                var buying = input.purchase;
+                var qnty = input.quantity;
+
+                console.log("you're trying to purchase ID # " + buying);
+                console.log("quantity: " + qnty);
+                console.log('checking our inventory...');
+
+                if (res[buying].stock_quantity > qnty) {
+                    connection.query(
+                        'UPDATE products SET ? WHERE ?',
+                        [
+                            { stock_quantity: (res(buying).stock_quantity - qnty) }
+                        ], (err, res) => {
+                            if (err) throw error;
+                            console.log('congrats on your purchase!!')
+                        }
+                    )
+                } else {
+                    console.log('Sorry! Not enough inventory...');
+                };
+            }
+            )
+
+        }
+    }
+    )
+}
+connect();
